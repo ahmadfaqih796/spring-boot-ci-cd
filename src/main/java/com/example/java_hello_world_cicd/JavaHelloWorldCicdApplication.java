a63cd4584c01911d226f23b43cd40061ab97101e
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-06-18 07:56:55 +0700

    commit

---
+++ src/main/java/com/example/java_hello_world_cicd/JavaHelloWorldCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_hello_world_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHelloWorldCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHelloWorldCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_hello_world_cicd/JavaHelloWorldCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_hello_world_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHelloWorldCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
